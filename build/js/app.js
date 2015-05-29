71c15f5439f03ef9f99e5f9b63323ac0f61ffee6
Mike Joyce <contact@mikejoyce.me>
2015-05-30 03:59:25 +0700

    Add build files

--- build/js/app.js
+++ build/js/app.js
@@ -9411,7 +9411,6 @@ var ViewModel = function() {
 	this.appName = "App Name";
 	this.contentName = "Content Name";
 	this.info = ko.observable(false);
-	this.latlang;
 	this.placeList = ko.observableArray([]); 
 	this.searchQuery = ko.observable('');
 	this.searchRadii = ko.observable('5000');
@@ -9489,11 +9488,11 @@ var Place = function(data) {
 	this.isHidden = ko.observable(false);
 }
 
-// Google Map
+// Google Map Constructor
 var Map = function() {
 
 	var map;
-	var pos;
+	var latLang;
 	var places = [];
 	var markers = [];
 
@@ -9511,19 +9510,19 @@ var Map = function() {
 	  if(navigator.geolocation) {
 	    navigator.geolocation.getCurrentPosition(function(position) {
 	
-	      pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+	      latLang = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 	 
 	      var infowindow = new google.maps.InfoWindow({
 	        map: map,
-	        position: pos,
-	        center: pos,
+	        position: latLang,
+	        center: latLang,
 	        content: 'Location found using HTML5.'
 	      });
 
-	      map.setCenter(pos);
+	      map.setCenter(latLang);
 
 		    var marker = new google.maps.Marker({
-		    	position: pos,
+		    	position: latLang,
 		    	map: map,
 		    	icon: { path: fontawesome.markers.MAP_MARKER,
 								fillColor: '#ffff00',
@@ -9568,7 +9567,7 @@ var Map = function() {
 	}
 
 	this.panTo = function() {
-		map.panTo(pos);
+		map.panTo(latLang);
 	}
 
 	this.zoomIn = function() {
@@ -9595,7 +9594,7 @@ var Map = function() {
     place = place.replace(/"/g, "");
 
     var request = {
-    	location: pos,
+    	location: latLang,
     	radius: radii,
     	types: []
     }
