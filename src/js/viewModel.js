a2fe2859c65b58f48cc27a20fba46f1d1659a81c
Mike Joyce <contact@mikejoyce.me>
2015-06-02 20:31:53 +0700

    Add Ion Range Slider init function

--- src/js/viewModel.js
+++ src/js/viewModel.js
@@ -26,6 +26,7 @@ var globals = {
 		this.placeList = ko.observableArray([]); 
 		this.searchQuery = ko.observable();
 		this.searchRadius = ko.observable('5000');
+		this.rangeSlider = $('#rangeSlider');
 
 		placesData.forEach(function(placeItem) {
 			self.placeList.push(new Place(placeItem));
@@ -78,6 +79,20 @@ var globals = {
 				self.placeList()[i].isHidden(false);
 			}
 		}
+
+		$(rangeSlider).ionRangeSlider({
+				grid: true,
+		    min: 1000,
+		    max: 5000,
+		    from: 2000,
+		    step: 1000,
+		    postfix: ' km',
+		    hide_min_max: true,
+		    prettify_enabled: true,
+		    prettify: function(num) {
+		    	return num / 1000;
+		    }
+		});
 	  
 	}
 
