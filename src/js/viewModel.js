b6c003d7eeb2048506f14dca397c105e50e0c09e
Mike Joyce <contact@mikejoyce.me>
2015-06-07 01:58:06 +0700

    Disable street view control

--- src/js/viewModel.js
+++ src/js/viewModel.js
@@ -114,6 +114,7 @@ var globals = {
 	    var mapOptions = {
 	      zoom: 13,
 	      zoomControl: false,
+	      streetViewControl: false,
 	      styles: [{"featureType":"administrative","elementType":"all","stylers":[{"visibility":"on"},{"lightness":33}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2e5d4"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#c5dac6"}]},{"featureType":"poi.park","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":20}]},{"featureType":"road","elementType":"all","stylers":[{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"color":"#c5c6c6"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#e4d7c6"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#fbfaf7"}]},{"featureType":"water","elementType":"all","stylers":[{"visibility":"on"},{"color":"#acbcc9"}]}]
 	    };
 
