77f3f3f1da2b9ecfc81ee548e2a91d485b7c2d04
Mike Joyce <contact@mikejoyce.me>
2015-08-14 05:08:07 +0700

    Chore: Update code comments

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,13 @@
 var gulp = require('gulp');
 var gulpLoadPlugins = require('gulp-load-plugins');
 var plugins = gulpLoadPlugins({
-		pattern: ['gulp-*', 'gulp.*', 'main-bower-files'],
+	pattern: ['gulp-*', 'gulp.*', 'main-bower-files'],
 	replaceString: /\bgulp[\-.]/
 });
 var path = require('path');
 
 // Scripts Task
-// Uglifies / Concats
+// Uglifies / Concatenates
 gulp.task('scripts', function() {
 	gulp.src(plugins.mainBowerFiles().concat(['src/js/mapMarkers.js', 'src/js/dataModel.js', 'src/js/viewModel.js'])) 		
 		.pipe(plugins.filter('*.js'))
@@ -18,7 +18,7 @@ gulp.task('scripts', function() {
 });
 
 // Styles Task
-// Uglifies
+// Uglifies / Concatenates
 gulp.task('compass', function() {
   gulp.src('./src/sass/*.scss')
     .pipe(plugins.compass({
@@ -34,7 +34,7 @@ gulp.task('compass', function() {
 });
 
 // Watch Task
-// Watches for Changes in JS / SASS / HTML Files and reload
+// Watches for changes in JS / SASS / HTML Files and reloads the page
 // Requires the LiveReload browser extension: http://livereload.com
 gulp.task('watch', function() {
 
