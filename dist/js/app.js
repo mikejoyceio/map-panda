0dfa246f8be681579b1d5824c3802080dc2da499
Mike Joyce <contact@mikejoyce.me>
2015-09-01 05:31:16 +0700

    Chore: Add build files

--- dist/js/app.js
+++ dist/js/app.js
@@ -11501,7 +11501,7 @@ var globals = {
 		this.mapInfo = ko.observable(false);
 		this.placeList = ko.observableArray([]); 
 		this.searchQuery = ko.observable();
-		this.searchRadius = ko.observable('5000');
+		this.searchRadius = ko.observable(5000);
 		this.rangeSlider = $('#rangeSlider');
 
 		placesData.forEach(function(placeItem) {
