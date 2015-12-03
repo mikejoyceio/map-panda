4e561734fc029c49671ced78cec382bee4b760d7
Mike Joyce <contact@mikejoyce.me>
2015-12-03 09:34:38 +0700

    Docs: Add JSDoc style code comments

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,38 @@
+/**
+ * @file Gulpfile
+ * @file overview Gulp build configuration
+ * @author Mike Joyce [hello@mikejoyce.io]
+ * @external 'gulp'
+ * @see {@link http://gulpjs.com/}
+ */
 
-// Gulp File
-
+/**
+ * Load Gulp
+ * @type {object}
+ */
 var gulp = require('gulp');
+/**
+ * Load Gulp Plugins
+ * @type {object}
+ * @external 'gulpLoadPlugins'
+ * @see {@link https://www.npmjs.com/package/gulp-load-plugins}
+ */
 var gulpLoadPlugins = require('gulp-load-plugins');
 var plugins = gulpLoadPlugins({
 	pattern: ['gulp-*', 'gulp.*', 'main-bower-files'],
 	replaceString: /\bgulp[\-.]/
 });
+/**
+ * Load Gulp Path 
+ * @type {object}
+ */
 var path = require('path');
 
-// Scripts Task
-// Uglifies / Concatenates
+/**
+ * Scripts Task. Uglifies & concatenates JavaScript files
+ * @external 'gulp.task'
+ * @see {@link https://github.com/gulpjs/gulp/blob/master/docs/getting-started.md}
+ */
 gulp.task('scripts', function() {
 	gulp.src(plugins.mainBowerFiles().concat(['src/js/mapMarkers.js', 'src/js/dataModel.js', 'src/js/viewModel.js'])) 		
 		.pipe(plugins.filter('*.js'))
@@ -19,8 +41,11 @@ gulp.task('scripts', function() {
 		.pipe(gulp.dest('./dist/js'))	
 });
 
-// Styles Task
-// Uglifies / Concatenates
+/**
+ * Styles Task. Uglifies & concatenates scss files.
+ * @external 'gulp.task'
+ * @see {@link https://github.com/gulpjs/gulp/blob/master/docs/getting-started.md}
+ */
 gulp.task('compass', function() {
   gulp.src('./src/sass/*.scss')
     .pipe(plugins.compass({
@@ -34,4 +59,9 @@ gulp.task('compass', function() {
     .pipe(gulp.dest('../dist/css'))
 });
 
+/**
+ * Set the default Gulp task
+ * @external 'gulp.task'
+ * @see {@link https://github.com/gulpjs/gulp/blob/master/docs/getting-started.md}
+ */
 gulp.task('default', ['scripts', 'compass']);
