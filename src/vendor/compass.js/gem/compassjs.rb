b9884de367187b7ae225fa3e55befcda8cc3984d
Mike Joyce <contact@mikejoyce.me>
2015-12-09 14:22:25 +0700

    Chore: Remove vendor directory

--- src/vendor/compass.js/gem/compassjs.rb
+++
@@ -1,8 +0,0 @@
-# Used only for Ruby on Rails gem to tell, that gem contain `lib/assets` with
-# compass.js file.
-module CompassJs
-  module Rails
-    class Engine < ::Rails::Engine
-    end
-  end
-end
