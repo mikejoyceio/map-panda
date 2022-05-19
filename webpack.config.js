a642e0187bf1184eeec07f789b7838a2c52cfc51
mikejoyceio <contact@mikejoyce.me>
2022-05-20 01:45:19 +0700

    Add Babel

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,16 @@ module.exports = {
         ],
       },
       {
+        test: /\.m?js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
       },
